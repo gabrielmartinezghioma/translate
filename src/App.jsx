9dbeec0c48bc5a1e329efb41bb476f2daa8c4494
Gabriel Martinez <martinezgabrielalejandro4@gmail.com>
2023-03-04 21:35:16 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,60 @@
-import './App.css'
+import './App.css';
 import axios from 'axios';
-import { useState } from 'react';
+import {useState } from 'react';
 
 function App() {
+  const apiKey = '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';
+  const [res, setRes] = useState('');
+  const [textTranslate, setTextTranslate] = useState('');
+  const [isChange, setIsChange] = useState(true)
 
-  const apiKey = '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';
-
-  const text = 'Esta es una API que permite crear peliculas, actores y directores con sus respectivos generos'
-  
   const translateText = async () => {
-    const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-002/completions', {
-      prompt: `Translate this into 1. French, 2. Spanish and 3. English:\n\n ${text} \n\n1.`,
-      temperature: 0.3,
-      max_tokens: 100,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`,
+    const response = await axios.post(
+      'https://api.openai.com/v1/engines/text-davinci-002/completions',
+      {
+        prompt: `Translate this into ${isChange ? 'Spanish' : 'English'} :\n\n${textTranslate}\n\n`,
+        temperature: 0.3,
+        max_tokens: 2048,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
       },
-    });
-    return response.data.choices[0].text
-  }
-
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`,
+        },
+      }
+    );
+    return response.data.choices[0].text;
+  };
+  
   const myAsyncFunction = async () => {
     const translatedText = await translateText();
-    console.log(translatedText);
-  }
+    setRes(translatedText);
+  };
 
-  myAsyncFunction();
+  const handleSubimit = (e) => {
+    e.preventDefault();
+    setTextTranslate(e.target[0].value);
+    myAsyncFunction();
+  };
 
   return (
     <div className="App">
-
+      <form onSubmit={(e) => handleSubimit(e)}>
+        <input type="text" onChange={(e) => setTextTranslate(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+      <h2>{res}</h2>
+      <button onClick={() => { setIsChange(!isChange) }}>change</button>
+      <h1>{isChange === true ? 'espanol' : 'ingles'}</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
 
 
 // English (en)
