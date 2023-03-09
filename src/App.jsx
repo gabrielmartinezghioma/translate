4497f91ee8425bdbb17c88176859250208d1b4f5
Gabriel Martinez <martinezgabrielalejandro4@gmail.com>
2023-03-09 13:46:08 -0300

    advances

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import './App.css';
 import axios from 'axios';
-import {useState } from 'react';
+import { useState } from 'react';
+
 
 function App() {
   const apiKey = import.meta.env.VITE_BACKEND_KEY;
   const [res, setRes] = useState('');
   const [textTranslate, setTextTranslate] = useState('');
-  const [isChange, setIsChange] = useState(true)
+  const [selectedOption, setSelectedOption] = useState('English');
+  const [resRender, setResRender] = useState([]);
 
   const translateText = async () => {
     const response = await axios.post(
       'https://api.openai.com/v1/engines/text-davinci-002/completions',
       {
-        prompt: `Translate this into ${isChange ? 'Spanish' : 'English'} :\n\n${textTranslate}\n\n`,
+        prompt: `Translate this into ${selectedOption} :\n\n${textTranslate}\n\n`,
         temperature: 0.3,
         max_tokens: 2048,
         top_p: 1.0,
@@ -28,27 +30,56 @@ function App() {
     );
     return response.data.choices[0].text;
   };
-  
+
   const myAsyncFunction = async () => {
     const translatedText = await translateText();
     setRes(translatedText);
+    setResRender(prevState => [...prevState, translatedText]);
   };
 
   const handleSubimit = (e) => {
     e.preventDefault();
     setTextTranslate(e.target[0].value);
-    myAsyncFunction();
+    myAsyncFunction();    
   };
 
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+
+
   return (
-    <div className="App">
+    <div className="flex flex-col justify-start items-center  min-h-screen overflow-hidden py-2 h-14 bg-gradient-to-r from-cyan-500 to-blue-500 ">
+      <select  onChange={handleOptionChange} className=" border-t-4  w-12 md:w-96 "  >
+        <option value="English">English (en)</option>
+        <option value="Arabic"> Arabic (ar)</option>
+        <option value="German">German (de)</option>
+        <option value="Spanish">Spanish (es)</option>
+        <option value="French">French (fr)</option>
+        <option value="option1">Hindi (hi)</option>
+        <option value="Italian"> Italian (it)</option>
+        <option value="Japanese">Japanese (ja)</option>
+        <option value="Korean">Korean (ko)</option>
+        <option value="Portuguese">Portuguese (pt)</option>
+        <option value="Russian"> Russian (ru)</option>
+        <option value="Simplified Chinese">Simplified Chinese (zh)</option>
+      </select>
+
       <form onSubmit={(e) => handleSubimit(e)}>
-        <input type="text" onChange={(e) => setTextTranslate(e.target.value)} />
-        <button type="submit">Submit</button>
+        <input type="text" maxLength="2000" onChange={(e) => setTextTranslate(e.target.value) } />
+        <button type="submit" className='border border-slate-300 hover:border-slate-400'>Submit</button>
       </form>
-      <h2>{res}</h2>
-      <button onClick={() => { setIsChange(!isChange) }}>change</button>
-      <h1>{isChange === true ? 'espanol' : 'ingles'}</h1>
+
+      <button onClick={()=>setResRender([])}>Clean</button>
+
+      <h2>{textTranslate.length}/2000</h2>
+
+      
+      {
+        resRender.map((text,index)=><p key={index} className="text-3xl font-bold underline">{text}</p>)
+      }
+     
     </div>
   );
 }
---
+++ src/apikey.js
@@ -0,0 +1,7 @@
+const apiKey= ()=>{
+  const key ='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'
+  return{
+    key
+  }
+}
+export default apiKey;
